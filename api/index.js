c7624ff0dbd0f6f9f02f2b49461669d93131a8bf
Marcelo Yeh <marceloyeh@gmail.com>
2025-11-26 00:12:45 -0300

    Express serverless oficial Vercel 2025

--- api/index.js
+++ api/index.js
@@ -1,24 +1,23 @@
 const express = require('express');
-const { VercelRequest, VercelResponse } = require('@vercel/node');
 const { engine } = require('express-handlebars');
 const path = require('path');
 
 const app = express();
 
-// Sua lógica de coleção (mantém igual)
+// Coleção temporária (sem fs, funciona no Vercel)
 app.use((req, res, next) => {
   req.user = { _id: 'user-test-123' };
   if (!app.locals.colecao) {
     app.locals.colecao = [
-      { ano: 1868, grau: 'MS63', precoPago: 300, moedaCatalogo: { denominacao: '40 Réis Império' }, fotosUsuario: ['https://i.imgur.com/9kR3vXj.jpg'] },
-      { ano: 1870, grau: 'MS65', precoPago: 600, moedaCatalogo: { denominacao: '40 Réis Império' } },
+      { ano: 1868, grau: 'MS63', precoPago: 300, moedaCatalogo: { denominacao: '40 Réis Império', fotoAnverso: 'https://catalogoyeh.com.br/img/40reis-anv.jpg' }, fotosUsuario: ['https://i.imgur.com/9kR3vXj.jpg'] },
+      { ano: 1870, grau: 'MS65', precoPago: 600, moedaCatalogo: { denominacao: '40 Réis Império', fotoAnverso: 'https://catalogoyeh.com.br/img/40reis-anv.jpg' } },
     ];
   }
   req.colecaoMock = app.locals.colecao;
   next();
 });
 
-// Handlebars (mantém igual)
+// Handlebars
 app.engine('hbs', engine({
   extname: '.hbs',
   layoutsDir: path.join(__dirname, '../src/views/layouts'),
@@ -28,27 +27,25 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '../src/views'));
 
-// Middlewares (mantém igual)
+// Middlewares
 app.use(express.static('../public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Rotas (mantém igual)
+// Rotas
 app.use('/', require('../src/routes/colecao'));
 app.use('/', require('../src/routes/adicionar'));
 
-// Home (mantém igual)
+// Home
 app.get('/', (req, res) => {
   res.send(`
     <h1>Catalogoyeh 2026 – AO VIVO NO VERCEL!</h1>
     <p><a href="/minha-colecao">→ Minha Coleção</a> (${req.colecaoMock.length} moedas)</p>
     <p><a href="/adicionar">+ Adicionar moeda</a></p>
     <hr>
-    <p><strong>Está funcionando 100% no Vercel!</strong></p>
+    <p><strong>Está funcionando 100%!</strong></p>
   `);
 });
 
-// EXPORT PARA VERCEL (ESSA É A CHAVE – SEM ISSO DÁ 404)
-module.exports = (req, res) => {
-  return app(req, res);
-};
+// EXPORT PARA VERCEL (obrigatório para serverless)
+module.exports = app;
